842dade175dec4c00deff9c540002427d170ab09
ouu28 <znarimitu.hunaba2832@gmail.com>
2026-02-17 01:18:17 +0900

    Update trains.js

--- trains.js
+++ trains.js
@@ -1,1 +1,13 @@
-const trains = [];
+const trains = [{
+  id: "TEST",
+  schedule: [
+    {
+      name: "青森",
+      lat: 40.8222,
+      lng: 140.7474,
+      arr: "00:00",
+      dep: "23:59"
+    }
+  ]
+}
+];
